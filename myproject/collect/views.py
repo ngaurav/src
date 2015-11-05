cb6f76dc4032d750776bea094619c502ca130acd
Nishant Gaurav <nishantgaurav@icloud.com>
2015-11-05 11:08:30 +0000

    v0.4

--- myproject/collect/views.py
+++ myproject/collect/views.py
@@ -4,12 +4,12 @@ from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
 
 def add_page(request):
     if request.method == 'POST':
-        form = PageForm(request.POST)
+        form = CollectForm(request.POST)
         if form.is_valid():
             form.save(commit=True)
             return register(request)
         else:
             print form.errors
     else:
-        form = PageForm()
+        form = CollectForm()
     return render(request, 'collect/add_page.html', {'form': form})
